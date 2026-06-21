b03ef9d461f141346db5d4ccbf0df029f86ae8a7
Remy <remy.furniss@gmail.com>
2026-06-21 21:11:53 +0900

    Configure Terraform remote state backend

---
+++ terraform/main.tf
@@ -0,0 +1,107 @@
+
+
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "visitor-counter"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "visitor-counter-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic_lambda" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "visitor_counter" {
+
+  filename         = "../lambda.zip"
+  function_name    = "visitor-counter-tf"
+
+  role             = aws_iam_role.lambda_role.arn
+
+  handler          = "lambda_function.lambda_handler"
+
+  runtime          = "python3.13"
+
+  source_code_hash = filebase64sha256("../lambda.zip")
+}
+
+resource "aws_iam_role_policy" "dynamodb_policy" {
+  name = "visitor-counter-dynamodb-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = aws_dynamodb_table.visitor_counter.arn
+      }
+    ]
+  })
+}
+
+resource "aws_apigatewayv2_api" "visitor_api" {
+  name          = "visitor-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET"]
+    allow_headers = ["*"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "visitor_integration" {
+  api_id                 = aws_apigatewayv2_api.visitor_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.visitor_counter.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "visitor_route" {
+  api_id    = aws_apigatewayv2_api.visitor_api.id
+  route_key = "GET /visitor"
+
+  target = "integrations/${aws_apigatewayv2_integration.visitor_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.visitor_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.visitor_api.execution_arn}/*/*"
+}
+
---
+++ terraform/state.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "remy-resume-terraform-state"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
